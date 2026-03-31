b2dc0c614c401afa361e4f93ca47ae8dc6f1bdd8
stevvnsy <stevvnsy@gmail.com>
2026-03-31 22:19:02 +0200

    add icon tailwind

--- frontend/src/components/organisms/Topbar.tsx
+++ frontend/src/components/organisms/Topbar.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import { IconButton, ThemeToggle } from "../atoms";
 import { SearchBar } from "../molecules";
 import { cn } from "../../utils/cn";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 interface TopbarProps {
   searchValue: string;
@@ -31,21 +32,10 @@ export function Topbar({
       )}
     >
       <div className="flex flex-col gap-4 xl:flex-row xl:items-center">
-        <div className="flex items-center gap-3">
+        <div className="flex flex-1 items-center gap-3">
           <div className="xl:hidden">
             <IconButton onClick={onOpenMobileSidebar} aria-label="Ouvrir le menu">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                className="h-5 w-5"
-              >
-                <path d="M4 6h16" />
-                <path d="M4 12h16" />
-                <path d="M4 18h16" />
-              </svg>
+              <Bars3Icon className="h-5 w-5 text-gray-500 hover:text-gray-700" />
             </IconButton>
           </div>
 
