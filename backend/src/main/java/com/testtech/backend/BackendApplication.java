03708b92d08ffadf71ea6bdd09a5106680ca6b15
stevvnsy <stevvnsy@gmail.com>
2026-03-31 07:55:09 +0200

    add entity

--- backend/src/main/java/com/testtech/backend/BackendApplication.java
+++ backend/src/main/java/com/testtech/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.testtech.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.testtech.backend.domain.entity")
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/testtech/backend/domain/entity/Question.java
@@ -0,0 +1,51 @@
+package com.testtech.backend.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    private String answer;
+
+    @ManyToOne
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
